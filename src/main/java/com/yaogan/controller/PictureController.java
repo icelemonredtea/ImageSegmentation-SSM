9ae4b8d5c8fb023df4d5300004d8e6ab058a8354
icelemonredtea <972921934@qq.com>
2019-02-12 00:11:41 +0800

    完成ssm整合，测试查询页面

--- src/main/java/com/yaogan/controller/PictureController.java
+++ src/main/java/com/yaogan/controller/PictureController.java
@@ -1,8 +1,14 @@
 package com.yaogan.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+
+import com.yaogan.entity.Picture;
+import com.yaogan.service.PictureService;
 
 /**
  * 控制层
@@ -13,11 +19,17 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class PictureController {
 	
-	@RequestMapping(value="/error.action")
-	public ModelAndView itemList() {
-		ModelAndView mva = new ModelAndView();
-		mva.setViewName("/WEB-INF/jsp/error.jsp");
-		return mva;
+	@Autowired
+	private PictureService pictureService;
+	
+	
+	@RequestMapping(value="/solution.action")
+	public String itemList(Model model ) {
+		
+		List<Picture> pictures = pictureService.findAllPictureByQueryVo();
+		model.addAttribute("pictures", pictures);
+		
+		return "solution";
 	}
 	
 }
